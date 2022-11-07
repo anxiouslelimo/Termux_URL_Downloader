d374b3bc6cb7fd971adc33f6a2501dfaa9c694cc
Anxious Lelimo <k8s3lrxy@duck.com>
2022-11-07 07:03:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,10 @@ mkdir /data/data/com.termux/files/home/bin
 
 #Installing dependencies
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
-yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install yt-dlp && yes | pip install you-get && yes | pip install bs4
+yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install yt-dlp
 
-wget --no-check-certificate --output-document="/data/data/com.termux/files/usr/bin/rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
-chmod +x "/data/data/com.termux/files/usr/bin/rbtv-dl"
-
-wget --no-check-certificate --output-document="/data/data/com.termux/files/usr/bin/bandcamp-dl" "https://raw.githubusercontent.com/M3GABOY/bandcamp-dl/main/bandcamp-dl.py"
-chmod +x "/data/data/com.termux/files/usr/bin/bandcamp-dl"
 
 #Installing the script
-wget -P /data/data/com.termux/files/home/bin/ --no-check-certificate "https://raw.githubusercontent.com/M3GABOY/Termux_url_opener_all_in_one_downloader/master/termux-url-opener" 
+wget -P /data/data/com.termux/files/home/bin/ --no-check-certificate "https://gitlab.com/anxiouslelimo/Termux_url_opener_all_in_one_downloader/-/raw/master/termux-url-opener" 
 chmod +x "/data/data/com.termux/files/home/bin/termux-url-opener"
 termux-setup-storage
